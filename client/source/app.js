0e046f54e3276815011042c057bcfce4989d42f4
Guy Levi <guy@ironsrc.com>
2015-08-12 15:09:11 +0300

    angular-material

---
+++ client/source/app.js
@@ -0,0 +1,32 @@
+angular
+  .module('iSmusic', ['ngMaterial', 'ui.router', 'users'])
+  .config(function($compileProvider) {
+    $compileProvider.debugInfoEnabled(false);
+  })
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/users');
+
+    $stateProvider
+      .state('users', {
+          url: '/users',
+          templateUrl: 'users/view/users.html',
+          controller: 'UserController'
+      })
+  })
+  .config(function($mdThemingProvider, $mdIconProvider){
+
+      $mdIconProvider
+        .defaultIconSet('./assets/svg/avatars.svg', 128)
+        .icon('menu'       , 'assets/svg/menu.svg'        , 24)
+        .icon('share'      , 'assets/svg/share.svg'       , 24)
+        .icon('google_plus', 'assets/svg/google_plus.svg' , 512)
+        .icon('hangouts'   , 'assets/svg/hangouts.svg'    , 512)
+        .icon('twitter'    , 'assets/svg/twitter.svg'     , 512)
+        .icon('phone'      , 'assets/svg/phone.svg'       , 512);
+
+      $mdThemingProvider.theme('default')
+        .primaryPalette('brown')
+        .accentPalette('red');
+
+  });
--- client/source/js/controllers/MainCtrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-app.controller('MainCtrl', ['$rootScope', '$q', '$injector', '$state',
-    function ($rootScope, $q, $injector, $state) {
-        
-    }
-]);
