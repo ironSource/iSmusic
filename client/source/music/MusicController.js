2447eac66a55259f5945462bc1afcff6949020e5
Ido Schachter <idoschachter@Idos-MacBook-Pro.local>
2015-08-12 22:10:12 +0300

    music and friends are split into tabs. click to play search is in music

--- client/source/music/MusicController.js
+++ client/source/music/MusicController.js
@@ -1,21 +1,12 @@
 (function () {
     function MusicController($scope, musicService, $q, $log, $http, VideosService) {
-        //init()
-        function init() {
-            $scope.youtube = VideosService.getYoutube();
-            $scope.results = VideosService.getResults();
-            $scope.upcoming = VideosService.getUpcoming();
-            $scope.history = VideosService.getHistory();
-            $scope.playlist = true;
-        }
 
         $scope.search = function () {
             musicService.get($scope.searchQuery, 5)
                 .then(function (data) {
                     $scope.videos = data;
                     console.log(data[0]);
-                    $scope.video = data[1];
-                    //VideosService.launchPlayer(data[0].id, data[0].title);
+                    $scope.activeVideo = data[1];
                 });
         };
 
@@ -33,7 +24,12 @@
                     $scope.user = res.data;
                 });
 
-        }
+        };
+
+        $scope.play = function (newVideo) {
+            $scope.activeVideo = newVideo;
+        };
+
         $scope.$watch('searchQuery', function (newVal, oldVal) {
             if (oldVal === newVal) return;
             $log.info(newVal);
@@ -45,6 +41,6 @@
 
     angular
         .module('music')
-        .controller('musicController', ['$scope', 'musicService', '$q', '$log', '$http','VideosService', MusicController]);
+        .controller('musicController', ['$scope', 'musicService', '$q', '$log', '$http', 'VideosService', MusicController]);
 
 })();
