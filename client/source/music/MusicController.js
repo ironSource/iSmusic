95ca42fcb7500bcc81ac05a01cf949f0c4e3ba84
Ido Schachter <idoschachter@Idos-MacBook-Pro.local>
2015-08-12 16:30:22 +0300

    added resources to package.JSON

--- client/source/music/MusicController.js
+++ client/source/music/MusicController.js
@@ -1,7 +1,5 @@
 (function () {
     function MusicController($scope, musicService, $q) {
-        $scope.cock = "cock";
-
         musicService.get('stevie', 5)
             .then(function (data) {
                 $scope.video = data[0];
