a2b61380ac85db7f2558f62200f2b799ad9cde26
Ido Schachter <idoschachter@Idos-MacBook-Pro.local>
2015-08-12 16:50:14 +0300

    search on keydown

--- client/source/music/MusicController.js
+++ client/source/music/MusicController.js
@@ -1,16 +1,22 @@
 (function () {
-    function MusicController($scope, musicService, $q) {
+    function MusicController($scope, musicService, $q, $log) {
 
-        $scope.search = function (){
+        $scope.search = function () {
             musicService.get($scope.searchQuery, 5)
                 .then(function (data) {
                     $scope.video = data[0];
                 });
-        }
+        };
+
+        $scope.$watch('searchQuery', function (newVal, oldVal) {
+            if (oldVal === newVal) return;
+            $log.info(newVal);
+            $scope.search(newVal);
+        });
     }
 
     angular
         .module('music')
-        .controller('musicController', ['$scope','musicService', '$q', MusicController]);
+        .controller('musicController', ['$scope', 'musicService', '$q', '$log',MusicController]);
 
 })();
